f8c51013f4653b0b89d559b6f314af9f68f60d24
Jordan Bean <jordanbean@microsoft.com>
2021-06-03 09:15:46 -0500

    keys

--- terraform/appInsights.tf
+++ terraform/appInsights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "appInsights" {
-  name                = "ai-${var.longName}"
+  name                = "ai-${local.longName}"
   resource_group_name = azurerm_resource_group.resourceGroup.name
   location            = azurerm_resource_group.resourceGroup.location
   application_type    = "web"
