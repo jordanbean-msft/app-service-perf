44bbbea1d9ec3705968a179d72bcb8b031c576f1
Jordan Bean <jordanbean@microsoft.com>
2021-06-03 10:24:57 -0500

    test

--- terraform/webApp.tf
+++ terraform/webApp.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service" "appService" {
   identity {
     type = "SystemAssigned"
   }
-  app_settings dddddddx{
+  app_settings ddddddx{
     APPINSIGHTS_INSTRUMENTATIONKEY        = azurerm_application_insights.appInsights.instrumentation_key
     APPLICATIONINSIGHTS_CONNECTION_STRING = azurerm_application_insights.appInsights.connection_string
   }
