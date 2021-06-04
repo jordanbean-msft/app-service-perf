58cc9dc6b06e49d17c0a96bea3ec1a38f687a56a
Jordan Bean <jordanbean@microsoft.com>
2021-06-03 22:46:48 -0500

    redis

---
+++ terraform/redis.tf
@@ -0,0 +1,9 @@
+resource "azurerm_redis_cache" "redisCache" {
+  name = "redis-${local.longName}"
+  resource_group_name = azurerm_resource_group.resourceGroup.name
+  location = azurerm_resource_group.resourceGroup.location
+  capacity = 1
+  family = "C"
+  sku_name = "Basic"
+  minimum_tls_version = "1.2"
+}
