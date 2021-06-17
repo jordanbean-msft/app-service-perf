dd8dc2ff7cd5526ed1089542576dc8bdf7921677
Jordan Bean <jordanbean@microsoft.com>
2021-06-17 18:22:24 -0500

    trap

--- ado/deploySqlModels.sh
+++ ado/deploySqlModels.sh
@@ -13,9 +13,8 @@ done
 
 for sqlScript in "$migrationScriptPath"/*.sql; do
   echo "Executing SQL migration script $sqlScript..."
-  trap '/opt/mssql-tools/bin/sqlcmd -S tcp:$serverName.database.windows.net -d $databaseName -U $username -P $password -r -R -i "$sqlScript"' ERR
-  catch() {
-    exit 1
-  }
+  if ! /opt/mssql-tools/bin/sqlcmd -S tcp:$serverName.database.windows.net -d $databaseName -U $username -P $password -r -R -i "$sqlScript"
+  then break
+  fi
   echo "Executed SQL migration script $sqlScript."
 done
